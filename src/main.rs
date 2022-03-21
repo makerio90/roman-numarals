df468e30f31655d34b7e740f5f7a24e5fdcdcf4a
makerio90 <makerio1@pm.me>
2022-03-21 16:12:03 -0400

    working on encodeing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
-use std::{env, fs};
+use std::{env};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    let filename = &args[1];
-
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-
-        println!("{}", ronum_decode(contents));
+    if args.len() < 3{
+        eprintln!("not enough arguments");
+    } else {
+        if args[1] == "decode"{
+            let input = &args[2];
+            println!("{}", ronum_decode(input.to_string()));
+        } else if args[1] == "encode"{
+            let input: u32 = args[2].trim().parse().expect("not a number");
+            println!("{}", ronum_encode(input))
+        } else {
+            eprintln!("plese select eiether encode or decode");
+        }
+    }
+}
+fn ronum_encode(mut num: u32) -> String{
+    let mut roman: Vec<&str> = vec![];
+    let numarals = [("m",1000), ("d",500), ("c",100), ("l",50), ("x",10), ("v",5), ("i",1)];
+    for (numarals,values) in numarals{
+        let mut i = num / values;
+        println!("{}", i * values);
+        num -= i * values;
+        while i > 0 {
+            roman.push(numarals);
+            i -= 1;
+        }
+    }
+    let roman: String = simplify(roman).to_string();
+    "test".to_string()
+}
+//fixme
+fn simplify(roman: Vec<&str>) -> Vec<&str>{
+    roman
 }
 fn ronum_decode(num: String) -> u32{
     let num = num.to_lowercase();
